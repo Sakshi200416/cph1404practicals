ed8ba0efbe7850a27f089f95802b166bb4511400
Sakshi200416 <sakshi.sharma@my.jcu.edu.au>
2024-10-13 21:34:25 +1100

    added prac3

---
+++ password_checker.py
@@ -0,0 +1,38 @@
+MIN_LENGTH = 2
+MAX_LENGTH = 6
+IS_SPECIAL_CHARACTER_REQUIRED = False
+SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+
+
+def main():
+    print("Please enter a valid password")
+    print(f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:")
+    print("\t1 or more uppercase characters")
+    print("\t1 or more lowercase characters")
+    print("\t1 or more numbers")
+    if IS_SPECIAL_CHARACTER_REQUIRED:
+        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
+        password = input("> ")
+    print(f"Your {len(password)}-character password is valid: {password}")
+
+
+def is_valid_password(password):
+    """Determine if the provided password is valid."""
+
+    number_of_lower = 0
+    number_of_upper = 0
+    number_of_digit = 0
+    number_of_special = 0
+    for character in password:
+        pass
+
+
+
+    # if we get here (without returning False), then the password must be valid
+    return True
+
+
+main()
